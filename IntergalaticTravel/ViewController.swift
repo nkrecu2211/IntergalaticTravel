f5f18ea8333b551e5120758377372924c1a74739
Nikolas Krecu <nkrecu2211@jh-mba158-07.d214.org>
2018-11-01 11:22:53 -0500

    Initial Commit

---
+++ IntergalaticTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntergalaticTravel
+//
+//  Created by Nikolas Krecu on 11/1/18.
+//  Copyright © 2018 John Hersey Highschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
