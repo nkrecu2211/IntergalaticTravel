9bfdf1c06a378dbaf591db24cd689776f679080f
Nikolas Krecu <nkrecu2211@jh-mba158-07.d214.org>
2018-11-02 11:38:28 -0500

    add image view to second view controller

---
+++ IntergalaticTravel/STARViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StarViewController.swift
+//  IntergalaticTravel
+//
+//  Created by Nikolas Krecu on 11/2/18.
+//  Copyright © 2018 John Hersey Highschool. All rights reserved.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
